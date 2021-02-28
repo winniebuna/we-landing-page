ec17f3cc173b45f32d59e378cb6922a25208025f
Oritsegbubemi <gbubemimakpokpomi@gmail.com>
2021-02-28 11:36:10 +0100

    worked on contact form

--- contactform.php
+++ contactform.php
@@ -1,5 +1,15 @@
 <?php
-if($_POST["submit"]) {
-    mail("gbubemimakpokpomi@gmail.com", "Form to email message", $_POST["message"], $_POST["senderEmail"],);
-}
+    if(isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $mailFrom = $_POST['email'];
+        $message = $_POST['message'];
+
+        $subject = "WomENpreneurs Contact Form";
+        $mailTo = "oritsegbubemi.makpokpomi@stu.cu.edu.ng";
+        $headers = "From: ".$mailFrom;
+        $txt = "You have received and email from ".$name.".\n\n".$message;
+
+        mail($mailTo, $subject, $txt, $headers);
+        header("Location: index.html?mailsent");
+    }
 ?>
